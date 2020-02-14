51fe44f09410fab7c2b63890687572704b4e4486
Andrew Po <ensimgen@gmail.com>
2020-02-13 16:22:23 -0800

    Text Analysis is done

---
+++ TextGeneratorTask.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace TextAnalysis
+{
+    static class TextGeneratorTask
+    {
+        public static string ContinuePhrase(
+            Dictionary<string, string> nextWords,
+            string phraseBeginning,
+            int wordsCount)
+        {
+            int i = 0;
+
+            if (wordsCount > 0)
+            {
+                while (i < wordsCount)
+                {
+                    var arrPhB = phraseBeginning.Split(' ');
+                    int lenPhr = arrPhB.Length;
+                    if (lenPhr >= 2 && nextWords.ContainsKey(arrPhB[lenPhr
+                            - 2] + " " + arrPhB[lenPhr - 1]))
+                        phraseBeginning = phraseBeginning + " " +
+                            nextWords[arrPhB[lenPhr
+                            - 2] + " " + arrPhB[lenPhr - 1]];
+                    else if (nextWords.ContainsKey(arrPhB[lenPhr - 1]))
+                        phraseBeginning = phraseBeginning + " " +
+                            nextWords[arrPhB[lenPhr - 1]];
+                    else
+                        return phraseBeginning;
+                    i++;
+                }
+            }
+
+            return phraseBeginning;
+        }
+    }
+}
